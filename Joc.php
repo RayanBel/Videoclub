b1cb0f36236c0870184006ef57778bfe1ff3359e
Edwin Espinoza <edwin.smx2@gmail.com>
2025-10-22 17:18:12 +0200

    Ejercicio 129 completado

---
+++ Joc.php
@@ -0,0 +1,41 @@
+<?php
+
+class Joc extends Soport
+{
+    // atributos
+    public string $consola;
+    private string $minNumJugadors;
+    private string $maxNumJugadors;
+
+    // cosntructor
+    public function __construct(string $titol, int $numero, float $preu, string $consola, string $minNumJugadors, string $maxNumJugadors)
+    {
+        // invocar el constructor de la clase padre
+        parent::__construct($titol, $numero, $preu);
+
+        // atributos de clase hijo
+        $this->consola = $consola;
+        $this->minNumJugadors = $minNumJugadors;
+        $this->maxNumJugadors = $maxNumJugadors;
+    }
+
+    // mostrar jugadores posibles para el juego
+    public function mostraJugadorsPossibles()
+    {
+        // dependiendo del maximo de jugadores, se muestra el mensaje para "1 jugador" o "x jugadores"
+        echo $this->maxNumJugadors >= 1 ? "Per a un jugador" : "Per a " . $this->maxNumJugadors . " jugadores<br>";
+    }
+
+    // sobreescribir metodo resumen
+    public function mostraResum()
+    {
+        // mostrar ccnsola
+        echo "<br>Joc per: " . $this->consola;
+
+        // invocar metodo padre
+        parent::mostraResum();
+
+        // msotrar numero de posibles jugadores
+        echo $this->mostraJugadorsPossibles();
+    }
+}
--- inici.php
+++ inici.php
@@ -24,3 +24,12 @@ echo "<strong>" . $elMeuDvd->titol . "</strong>";
 echo "<br>Preu: " . $elMeuDvd->getPreu() . " euros";
 echo "<br>Preu IVA inclos: " . $elMeuDvd->getPreuAmbIVA() . " euros";
 $elMeuDvd->mostraResum();
+
+
+include "Joc.php";
+
+$elMeuJoc = new Joc("The Last of Us Part II", 26, 49.99, "PS4", 1, 1);
+echo "<strong>" . $elMeuJoc->titol . "</strong>";
+echo "<br>Preu: " . $elMeuJoc->getPreu() . " euros";
+echo "<br>Preu IVA inclos: " . $elMeuJoc->getPreuAmbIVA() . " euros";
+$elMeuJoc->mostraResum();
